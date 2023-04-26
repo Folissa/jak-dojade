04ba12e8df2d01959cb73716c514012434bc5169
Anna Berkowska <annaberkowska@protonmail.com>
2023-04-26 22:46:58 +0200

    Add constants

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 
 #define COORDINATES_SIZE 2
+#define X_COORDINATE 0
+#define Y_COORDINATE 1
+
 #define EMPTY_INT (-1)
 
 int main() {
@@ -16,6 +19,7 @@ int main() {
     // https://linuxhint.com/two-dimensional-array-malloc-c-programming/
     char *map = (char *)malloc((height * width) * sizeof(char));
 
+    // Create cities coordinates array
     int citiesCount = 0;
     int *citiesCoordinates = (int *)malloc(((height * width) * COORDINATES_SIZE) * sizeof(int));
 
@@ -24,14 +28,16 @@ int main() {
         for (int x = 0; x < width; x++) {
             scanf(" %c", &input);
             *(map + y * width + x) = input;
-            // If this is a city
+            // If this is a city, add its coordinates to the citiesCoordinates array
             if (input == '*') {
-                *(citiesCoordinates + citiesCount * COORDINATES_SIZE + 0) = x;
-                *(citiesCoordinates + citiesCount * COORDINATES_SIZE + 1) = y;
+                *(citiesCoordinates + citiesCount * COORDINATES_SIZE + X_COORDINATE) = x;
+                *(citiesCoordinates + citiesCount * COORDINATES_SIZE + Y_COORDINATE) = y;
                 citiesCount++;
             }
         }
     }
+
+    const int maximalCityNameLength = width;
 
     // Output the data
     for (int y = 0; y < height; y++) {
@@ -41,8 +47,9 @@ int main() {
         printf("\n");
     }
 
+    // Output the coordinates of the cities
     for (int i = 0; i < citiesCount; i++) {
-        printf("(%d, %d)\n", *(citiesCoordinates + i * COORDINATES_SIZE + 0), *(citiesCoordinates + i * COORDINATES_SIZE + 1));
+        printf("(%d, %d)\n", *(citiesCoordinates + i * COORDINATES_SIZE + X_COORDINATE), *(citiesCoordinates + i * COORDINATES_SIZE + Y_COORDINATE));
     }
 
     free(map);
