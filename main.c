fd1a4c82b6a7f635cedf7a68d7cccb4c037b8f1f
Anna Berkowska <annaberkowska@protonmail.com>
2023-04-26 21:36:12 +0200

    Find cities coordinates

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define COORDINATES_SIZE 2
+#define EMPTY_INT (-1)
+
 int main() {
     int width, height;
 
@@ -11,25 +14,37 @@ int main() {
     // Create a map
     char input;
     // https://linuxhint.com/two-dimensional-array-malloc-c-programming/
-    char *arrayOfChars = (char *)malloc((width * height) * sizeof(char));
+    char *map = (char *)malloc((height * width) * sizeof(char));
 
-    
+    int citiesCount = 0;
+    int *citiesCoordinates = (int *)malloc(((height * width) * COORDINATES_SIZE) * sizeof(int));
+
     // Input the data
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
             scanf(" %c", &input);
-            *(arrayOfChars + y * width + x) = input;
+            *(map + y * width + x) = input;
+            // If this is a city
+            if (input == '*') {
+                *(citiesCoordinates + citiesCount * COORDINATES_SIZE + 0) = x;
+                *(citiesCoordinates + citiesCount * COORDINATES_SIZE + 1) = y;
+                citiesCount++;
+            }
         }
     }
 
     // Output the data
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
-            printf("%c", *(arrayOfChars + y * width + x));
+            printf("%c", *(map + y * width + x));
         }
         printf("\n");
     }
 
-    free(arrayOfChars);
+    for (int i = 0; i < citiesCount; i++) {
+        printf("(%d, %d)\n", *(citiesCoordinates + i * COORDINATES_SIZE + 0), *(citiesCoordinates + i * COORDINATES_SIZE + 1));
+    }
+
+    free(map);
     return 0;
 }
